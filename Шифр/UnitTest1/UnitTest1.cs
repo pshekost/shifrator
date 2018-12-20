b6775888491ece2770992867d8141556a1db7563
Konstantin <32603190+pshekost@users.noreply.github.com>
2018-12-21 01:13:19 +0500

    Add files via upload

---
+++ Шифр/UnitTest1/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Шифр;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // исходные данные
+
+            int i = Convert.ToChar("r");
+            string expected = "p";
+            Program g = new Program();
+            string rezult = g.bit(i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // исходные данные
+
+            int i = Convert.ToChar("c");
+            string expected = "a";
+            Program g = new Program();
+            string rezult = g.bit(i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // исходные данные
+
+            int i = Convert.ToChar("6");
+            string expected = "4";
+            Program g = new Program();
+            string rezult = g.bit(i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // исходные данные
+
+            int i = Convert.ToChar("K");
+            string expected = "I";
+            Program g = new Program();
+            string rezult = g.bit(i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            // исходные данные
+
+            int i = Convert.ToChar("-");
+            string expected = "+";
+            Program g = new Program();
+            string rezult = g.bit(i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            // исходные данные
+            int i = Convert.ToChar("(");
+            string expected = "&";
+            Program g = new Program();
+            string rezult = g.bit(i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+    }
+}
---
+++ Шифр/Шифр/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шифр
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите строку: ");
+                string str = Console.ReadLine();
+                if (str == "")
+                {
+                    Console.WriteLine("Ошибка!");
+                }
+                else
+                {
+                    string[] split = str.Split(' ');
+                    string[] mas = split;
+                    Program p = new Program();
+                    int j = 0;
+                    foreach (var t in split)
+                    {
+
+                        string s = "";
+                        for (int i = 0; i <= t.Length - 1; i++)
+                        {
+                            s = s + p.bit(t[i]);
+                        }
+                        mas[j] = s;
+                        j++;
+                    }
+                    for (int i = 0; i <= mas.Length - 1; i++)
+                        Console.Write(mas[i] + " ");
+
+                }
+                Console.ReadLine();
+            }
+            
+        }
+        public string bit(int t)
+        {
+            int a = Convert.ToChar(t) - 2;
+            char ss = Convert.ToChar(a);
+            return Convert.ToString(ss);
+        }
+    }
+}
